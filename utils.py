9ae5d2dfa3f7c2a9571584a64fb46baef1a32b79
David Herdlicka <david.herdlicka@gmail.com>
2022-12-13 20:14:21 +0100

    feat: vercel.json

--- utils.py
+++ utils.py
@@ -1,5 +1,4 @@
 import torch
-import torch.nn as nn
 from torchvision.utils import save_image
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -46,7 +45,7 @@ def weights_init(m):
     """
     classname = m.__class__.__name__
     if classname.find('Conv') != -1:
-        nn.init.normal_(m.weight.data, 0.0, 0.02)
+        torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
     elif classname.find('BatchNorm') != -1:
-        nn.init.normal_(m.weight.data, 1.0, 0.02)
-        nn.init.constant_(m.bias.data, 0)
+        torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
+        torch.nn.init.constant_(m.bias.data, 0)
